5346e54004fb697af68f3e252c88266df485aabd
Dmytro Dzereniuk <dsdzerenyuk@gmail.com>
2019-12-04 23:10:54 +0200

    function added

--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
-print('Hello world')
-print('Adding new line')
-print('I won\'t stop in Python learning ')
+#print('Hello world')
+#print('Adding new line')
+#print('I won\'t stop in Python learning ')
+
+
+def factorial(n):
+    sum = 1
+    for i in range(1, n + 1):
+        sum *= i
+    return sum
+print(factorial(6))
